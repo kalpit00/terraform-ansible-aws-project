c786e3fedc216d9c1c826f7c5a46cdfa6c2a97cb
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-10-11 05:55:22 +0530

    Add route-table and to route all internet traffic from gateway

--- main.tf
+++ main.tf
@@ -18,3 +18,17 @@ resource "aws_vpc" "prod-vpc" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.prod-vpc.id
 }
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Prod"
+  }
+}
